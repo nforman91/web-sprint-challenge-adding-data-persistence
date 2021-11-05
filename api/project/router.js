d8d0e863313f6a7bb88920e8f6231f09f81d623c
Nathan <nathan.forman@gmail.com>
2021-11-04 18:37:45 -0700

    Got index and server files done and got server running

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,9 @@
 // build your `/api/projects` router here
+const express = require('express')
+const Projects = require('./model')
+
+const router = express.Router()
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,1 +1,9 @@
 // build your server here and require it from index.js
+const express = require('express')
+const server = express()
+server.use(express.json())
+
+const ProjectsRouter = require('./project/router')
+server.use('/api/projects', ProjectsRouter)
+
+module.exports = server
