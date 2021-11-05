f672a437e8e2747424cca2b50e8dbb1a02befc8b
Nathan <nathan.forman@gmail.com>
2021-11-05 13:45:39 -0700

    Cleaned up

--- api/project/model.js
+++ api/project/model.js
@@ -1,31 +1,8 @@
 const db = require('../../data/dbConfig')
 
 async function find(){
-    return db('projects')
-    // const rows = await db('projects')
-
-    // let result = { ...rows, project_completed: !!rows.project_completed }
-
-    // return result
-
-    // let rows.project_completed = 
-
-    // const result = {
-    //     project_id: rows.project_id,
-    //     project_name: rows.project_name,
-    //     project_description: rows.project_description,
-    //     project_completed: rows.project_completed,
-    // }
-
-    // rows.forEach(row => {
-    //     if(project_completed === 0){
-    //         !!result.project_completed
-    //     } else {
-    //         !!result.project_completed
-    //     }
-    // })
-
-    // return result
+    const rows = await db('projects')
+    return rows
 }
 
 function create(project){
